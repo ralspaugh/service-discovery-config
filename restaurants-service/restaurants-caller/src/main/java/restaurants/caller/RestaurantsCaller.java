76904d6b518a8ad58b51435b5f77909adad0dda5
Ryan Alspaugh <ryan.alspaugh@slalom.com>
2017-12-13 16:36:24 -0600

    Add configuration server and config repo

--- restaurants-service/restaurants-caller/src/main/java/restaurants/caller/RestaurantsCaller.java
+++ restaurants-service/restaurants-caller/src/main/java/restaurants/caller/RestaurantsCaller.java
@@ -1,6 +1,7 @@
 package restaurants.caller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import restaurants.client.RestaurantsClient;
@@ -11,8 +12,11 @@ public class RestaurantsCaller {
     @Autowired
     private RestaurantsClient client;
 
+    @Value("${caller.restaurant.name}")
+    private String restaurantName;
+
     @RequestMapping("/")
     public String index() {
-        return client.findRestaurantByName("Brunos On The Boulevard").toString();
+        return client.findRestaurantByName(restaurantName).toString();
     }
 }
