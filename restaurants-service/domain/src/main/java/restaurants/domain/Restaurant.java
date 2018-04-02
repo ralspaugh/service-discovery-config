cab0d654108d6e4ee860ff707a753711c45915aa
Ryan Alspaugh <ryan.alspaugh@slalom.com>
2018-04-02 13:25:45 -0500

    Version that runs on pivotal cloud foundry

--- restaurants-service/domain/src/main/java/restaurants/domain/Restaurant.java
+++ restaurants-service/domain/src/main/java/restaurants/domain/Restaurant.java
@@ -1,10 +1,12 @@
 package restaurants.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
+@Document(collection = "restaurants")
 public class Restaurant {
 
     @Id
--- restaurants-service/restaurants-caller/src/main/java/restaurants/Application.java
+++ restaurants-service/restaurants-caller/src/main/java/restaurants/Application.java
@@ -2,10 +2,12 @@ package restaurants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "restaurants")
 @ComponentScan(basePackages = "restaurants")
 public class Application {
